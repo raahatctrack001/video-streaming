1befd3af619601d52aadbb9d4455283062c1dac2
Raahat Khan <raahatctrack001.rc@gmail.com>
2024-11-07 23:04:32 +0530

    basic project setup

---
+++ src/app/(website)/_components/HomeHeader.tsx
@@ -0,0 +1,5 @@
+export default function HomeHeader(){
+    return <div>
+        its home header
+    </div>
+}
---
+++ src/app/(website)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import HomeHeader from "./_components/HomeHeader";
+
+export default function Home() {
+  return (
+    <div>
+      <HomeHeader />
+      this is home page
+    </div>
+  );
+}
